77f9d7c3bfe0c007facd3f07039b44df4118a4f9
Ryan <110047631+ryanbroome@users.noreply.github.com>
2025-02-04 15:13:16 -0500

    fixed refresh page NOT FOUND error, SPA error using path

--- app.js
+++ app.js
@@ -4,6 +4,9 @@
 
 const express = require("express");
 const cors = require("cors");
+// *
+const path = require("path");
+// *
 
 const { NotFoundError } = require("./expressError");
 const { authenticateJWT } = require("./middleware/auth");
@@ -16,7 +19,7 @@ const locationRoutes = require("./routes/locations");
 
 const morgan = require("morgan");
 const app = express();
-// ?
+
 app.use(cors());
 app.use(express.json());
 app.use(morgan("tiny"));
@@ -28,21 +31,31 @@ app.use("/vehicles", vehiclesRoutes);
 app.use("/transactions", transactionRoutes);
 app.use("/locations", locationRoutes);
 
+// *
+app.use(express.static(path.join(__dirname + `client/build`)));
+// *
+
 /**  Handle 404 errors */
 app.use(function (req, res, next) {
-  return next(new NotFoundError());
+    return next(new NotFoundError());
 });
 
+// *
+//  the "catchall" handler: for any request that doesn't match one above. send back React's index.html file
+app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname + `/client/build/index.html`));
+});
+// *
+
 //   Catch all generic error handler
-
 app.use(function (err, req, res, next) {
-  if (process.env.NODE_ENV !== "test") console.error(err.stack);
-  const status = err.status || 500;
-  const message = err.message;
+    if (process.env.NODE_ENV !== "test") console.error(err.stack);
+    const status = err.status || 500;
+    const message = err.message;
 
-  return res.status(status).json({
-    error: { message, status },
-  });
+    return res.status(status).json({
+        error: { message, status },
+    });
 });
 
 module.exports = app;
