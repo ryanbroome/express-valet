04480e4ee8599f940ad341678cf7038c85c20eb0
Ryan <110047631+ryanbroome@users.noreply.github.com>
2025-02-08 10:39:56 -0500

    updated db to pool instead of pg

--- db.js
+++ db.js
@@ -1,23 +1,27 @@
 "use strict";
 /** Database setup for valet. */
-const { Client } = require("pg");
+const { Pool } = require("pg");
 const { getDatabaseUri } = require("./config");
 
-let db;
-
-if (process.env.NODE_ENV === "production") {
-  db = new Client({
+const db = new Pool({
     connectionString: getDatabaseUri(),
-    ssl: {
-      rejectUnauthorized: false,
-    },
-  });
-} else {
-  db = new Client({
-    connectionString: getDatabaseUri(),
-  });
-}
+    ssl:
+        process.env.NODE_ENV === "production"
+            ? {
+                  rejectUnauthorized: false,
+              }
+            : false,
+    connectionTimeoutMillis: 5000,
+    max: 20,
+});
 
-db.connect();
+// Add connection monitoring
+db.on("connect", () => {
+    console.log("Database connected");
+});
+
+db.on("error", (err) => {
+    console.error("Unexpected database error:", err);
+});
 
 module.exports = db;
