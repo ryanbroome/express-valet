c6a761e26a56c3c0f74d9f47a021cdc0ffbafba2
Ryan <110047631+ryanbroome@users.noreply.github.com>
2025-02-08 13:01:13 -0500

    updated config getDatabaseUri

--- config.js
+++ config.js
@@ -11,7 +11,7 @@ const PORT = +process.env.PORT || 3001;
 
 // Use dev database, testing database, or via env var, production database
 function getDatabaseUri() {
-  return process.env.NODE_ENV === "test" ? "valet_test" : process.env.DATABASE_URL || "valet";
+    return process.env.NODE_ENV === "test" ? "valet_test" : process.env.DATABASE_URL || "postgresql://postgres:parkpilot1master@park-pilot1.cv0u04cq2cvm.us-east-2.rds.amazonaws.com:5432/postgres";
 }
 
 // Speed up bcrypt during tests, since the algorithm safety isn't being tested
@@ -25,8 +25,8 @@ console.log("Database:".yellow, getDatabaseUri());
 console.log("---");
 
 module.exports = {
-  SECRET_KEY,
-  PORT,
-  BCRYPT_WORK_FACTOR,
-  getDatabaseUri,
+    SECRET_KEY,
+    PORT,
+    BCRYPT_WORK_FACTOR,
+    getDatabaseUri,
 };
