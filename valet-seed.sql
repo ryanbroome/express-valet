5b3d890a6bf3c4ef58b373d3c64251f91a2c2f41
Ryan <110047631+ryanbroome@users.noreply.github.com>
2025-07-02 15:19:03 -0400

    updated seed file to include more vehicles, transactions

--- valet-seed.sql
+++ valet-seed.sql
@@ -57,16 +57,47 @@ VALUES
     (1002, 2, '555-404-0404', 'Blue', 'Toyota', '[{"x":222.71875,"y":143}]', 'automatic'),
     (1003, 3, '555-505-0505', 'Green', 'Honda', '[{"x":222.71875,"y":143}]', 'manual'),
     (1004, 4, '555-606-0606', 'Black', 'Ford', '[{"x":222.71875,"y":143}]', 'automatic'),
-    (1005, 5, '555-707-0707', 'White', 'Chevrolet', '[{"x":222.71875,"y":143}]', 'stick shift');
+    (1005, 5, '555-707-0707', 'White', 'Chevrolet', '[{"x":222.71875,"y":143}]', 'stick shift'),
+    (1006, 6, '555-808-0808', 'Red', 'Nissan', '[{"x":222.71875,"y":143}]', 'automatic'),
+    (1007, 1, '555-909-0909', 'Purple', 'Kia', '[{"x":222.71875,"y":143}]', 'manual'),
+    (1008, 2, '555-010-1010', 'Orange', 'Hyundai', '[{"x":222.71875,"y":143}]', 'automatic'),
+    (1009, 3, '555-111-1212', 'Red', 'Subaru', '[{"x":222.71875,"y":143}]', 'stick shift'),
+    (1010, 4, '555-131-1414', 'Gray', 'Mazda', '[{"x":222.71875,"y":143}]', 'automatic'),
+    (1011, 5, '555-151-1616', 'Brown', 'Volkswagen', '[{"x":222.71875,"y":143}]', 'manual'),
+    (1012, 6, '555-171-1818', 'Blue', 'Toyota', '[{"x":222.71875,"y":143}]', 'stick shift'),
+    (1013, 1, '555-191-2020', 'Purple', 'Honda', '[{"x":222.71875,"y":143}]', 'automatic'),
+    (1014, 2, '555-212-2222', 'Green', 'Ford', '[{"x":222.71875,"y":143}]', 'manual'),
+    (1015, 3, '555-232-2424', 'Green', 'Chevrolet', '[{"x":222.71875,"y":143}]', 'stick shift'),
+    (1016, 4, '555-252-2626', 'Blue', 'Nissan', '[{"x":222.71875,"y":143}]', 'automatic'),
+    (1017, 5, '555-272-2828', 'Blue', 'Kia', '[{"x":222.71875,"y":143}]', 'manual'),
+    (1018, 6, '555-292-3030', 'White', 'Hyundai', '[{"x":222.71875,"y":143}]', 'stick shift'),
+    (1019, 1, '555-313-3232', 'Silver', 'Subaru', '[{"x":222.71875,"y":143}]', 'automatic'),
+    (1020, 2, '555-333-3434', 'Black', 'Mazda', '[{"x":222.71875,"y":143}]', 'manual'),
+    (1021, 3, '555-353-3636', 'Red', 'Volkswagen', '[{"x":222.71875,"y":143}]', 'stick shift'),
+    (1022, 4, '555-373-3838', 'Blue', 'Toyota', '[{"x":222.71875,"y":143}]', 'automatic'),
+    (1023, 5, '555-393-4040', 'Green', 'Honda', '[{"x":222.71875,"y":143}]', 'manual'),
+    (1024, 6, '555-414-4242', 'Black', 'Ford', '[{"x":222.71875,"y":143}]', 'automatic'),
+    (1025, 1, '555-434-4444', 'White', 'Chevrolet', '[{"x":222.71875,"y":143}]', 'stick shift'),
+    (1026, 2, '555-454-4646', 'Red', 'Nissan', '[{"x":222.71875,"y":143}]', 'automatic');
 
 -- 8. Seed transactions (example)
 INSERT INTO transactions (user_id, vehicle_id, podium_id, location_id, status_id, transaction_time, updated_at)
 VALUES
     (1, 1, 1, 1, 1, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
-    (2, 1, 2, 1, 2, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
-    (3, 1, 3, 1, 3, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
-    (4, 1, 4, 1, 4, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
-    (5, 1, 5, 1, 5, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP);
+    (2, 2, 2, 1, 2, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
+    (3, 3, 3, 1, 3, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
+    (4, 4, 4, 1, 4, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
+    (5, 5, 5, 1, 5, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
+    (6, 6, 1, 1, 6, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
+    (7, 7, 2, 1, 1, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
+    (8, 8, 3, 1, 2, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
+    (9, 9, 4, 1, 3, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
+    (10, 10, 5, 1, 4, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
+    (11, 11, 1, 1, 5, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
+    (12, 12, 2, 1, 6, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
+    (13, 13, 3, 1, 1, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
+    (1, 14, 4, 1, 2, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
+    (2, 15, 5, 1, 3, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP);
 
     -- 9. Seed surveys table
     INSERT INTO surveys (transaction_id, q1_response, q2_response, q3_response, q4_response, q5_response, q6_response, submitted_at)
